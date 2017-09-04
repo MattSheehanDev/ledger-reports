18595babfcb1b98f0845cfd00e0ebf8f75c824ca
Matt Sheehan <sheehamj@outlook.com>
2017-09-04 04:16:25 -0400

    added fi burndown

---
+++ reports.fi/fi-burndown.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+LEDGER_FILE="$HOME/Dropbox/journals/finances/accounting/ledger/data/general.ledger"
+LEDGER_PRICES="$HOME/Dropbox/journals/finances/accounting/ledger/data/prices.ledger"
+
+YEAR=2017
+MONTH=06
+
+BEGIN="2017/06"
+END="2017/07/01"
+NOW="2017/06/30"
+
+
+net_worth=$(\
+ledger bal "^Assets" "^Liabilities" -X $ --price-db ${LEDGER_PRICES} -f ${LEDGER_FILE} \
+--now ${NOW} -e ${END} --current \
+--balance-format "%(quantity(display_total))\n" \
+| tail -n 1 | sed -e 's/^[ \t]*//' \
+)
+withdrawal_rate="3.85"
+growth_rate="5.00"
+
+
+pformat="%-30s %12s\n"
+printf "$pformat" "Net worth" $(printf "$%'.2f" $net_worth)
+printf "$pformat" "Withdrawal rate" $(printf "%'.2f%%" $withdrawal_rate)
+printf "$pformat" "Inflation-adjusted growth rate" $(printf "%'.2f%%" $growth_rate)
+printf "\n" {1}
+
+
+pformat="%-11s %-11s %-11s %-14s %-30s\n"
+printf "$pformat" "Daily" "Monthly" "Annually" "Savings required" ""
+printf '=%.0s' {1..80}
+printf "\n"
+
+annual_expenses=$(ledger bal "/^Expenses:(?!Tax|Deductions)/" "/^Expenses:Tax:Sales/" -X $ \
+-f ${LEDGER_FILE} --price-db ${LEDGER_PRICES} --no-total \
+--now $NOW -b "$YEAR/01/01" -e $END --current \
+--balance-format "\
+%-12((abs(display_total / $MONTH) * 12) / 365)\
+%-12((abs(display_total / $MONTH) * 12) / 12)\
+%-12(abs(display_total / $MONTH) * 12)\
+%-14((abs(display_total / $MONTH) * 12) / ($withdrawal_rate / 100))\
+%(depth_spacer)\
+%-30(truncated(partial_account(options.flat), int(20), int(2)))\n\
+"\
+)
+echo "$annual_expenses"
+
+
+# pformat="%-12 %-12s %-12s %-12s %-12s %-20s\n"
+# printf "$pformat" "Average" "Daily" "Monthly" "Annually" "Required Savings"
+# printf '=%.0s' {1..80}
+# printf "\n"
+
+
+# ledger bal "/^Expenses:(?!Tax|Deductions)/" "/^Expenses:Tax:Sales/" -X $ \
+# -f ${LEDGER_FILE} --price-db ${LEDGER_PRICES} --no-total \
+# --now $NOW -b "$YEAR/01/01" -e $END --current \
+# --balance-format "\
+# %-12(abs(display_total / $MONTH))\
+# %-12((abs(display_total / $MONTH) * 12) / 365)\
+# %-12((abs(display_total / $MONTH) * 12) / 12)\
+# %-12(abs(display_total / $MONTH) * 12)\
+# %-12((abs(display_total / $MONTH) * 12) / ($withdrawal_rate / 100))\
+# %(depth_spacer)\
+# %-20(truncated(partial_account(options.flat), int(20), int(2)))\n\
+# "
+
+
+# annual_expenses=$(\
+# ledger bal "/^Expenses:(?!Tax|Deductions)/" "/^Expenses:Tax:Sales/" -X $ \
+# -f ${LEDGER_FILE} --price-db ${LEDGER_PRICES} \
+# --now $NOW -b "$YEAR/01/01" -e $END --current \
+# --balance-format "%(abs(quantity(display_total / $MONTH)) * 12)\n" \
+# | tail -n -1 | sed -e 's/^[ \t]*//' \
+# )
+# monthly_expenses=$(echo "($annual_expenses / 12)" | bc -l)
+# daily_expenses=$(echo "($annual_expenses / 365)" | bc -l)
+# required_savings=$(echo "($annual_expenses / ($withdrawal_rate / 100))" | bc -l)
+
+
+# pformat="%-20s %-20s %-20s %-20s\n"
+# printf "$pformat" "Daily" "Monthly" "Annually" "Required Savings"
+# printf "$pformat" $(printf "$%'.2f" $daily_expenses) $(printf "$%'.2f" $monthly_expenses) $(printf "$%'.2f" $annual_expenses) $(printf "$%'.2f" $required_savings)
+# printf "\n" {1}
+
+
+# annual_expenses=$(\
+# ledger bal "/^Expenses:(?!Tax|Deductions)/" "/^Expenses:Tax:Sales/" -X $ \
+# -f ${LEDGER_FILE} --price-db ${LEDGER_PRICES} \
+# --now $NOW -b "$YEAR/01/01" -e $END --current \
+# --balance-format "%(abs(quantity(display_total / $MONTH)) * 12)\n" \
+# | tail -n -1 | sed -e 's/^[ \t]*//' \
+# )
--- test.sh
+++ test.sh
@@ -12,39 +12,81 @@
 # # %-14(strip(display_total)) \
 # # %-10(lot_price)\n"
 
+
+LEDGER_FILE="$HOME/Dropbox/journals/finances/accounting/ledger/data/general.ledger"
+LEDGER_PRICES="$HOME/Dropbox/journals/finances/accounting/ledger/data/prices.ledger"
+
 YEAR=2017
-# MONTH=07
 MONTH=06
 
-current_date="2017/07"
-until_date="2017/08/01"
-now_date="2017/07/31"
-# current_date="2017/07/01"
+BEGIN="2017/06"
+END="2017/07/01"
+NOW="2017/06/30"
+
+pformat="%-11s %-11s %-11s %-14s %-30s\n"
+printf "$pformat" "Daily" "Monthly" "Annually" "Savings required" ""
+printf '=%.0s' {1..80}
+printf "\n"
+
+withdrawal_rate="3.85"
+
+ledger bal "/^Expenses:(?!Tax|Deductions)/" "/^Expenses:Tax:Sales/" -X $ \
+-f ${LEDGER_FILE} --price-db ${LEDGER_PRICES} --no-total \
+--now $NOW -b "$YEAR/01/01" -e $END --current \
+--balance-format "\
+%-12((abs(display_total / $MONTH) * 12) / 365)\
+%-12((abs(display_total / $MONTH) * 12) / 12)\
+%-12(abs(display_total / $MONTH) * 12)\
+%-14((abs(display_total / $MONTH) * 12) / ($withdrawal_rate / 100))\
+%(depth_spacer)\
+%-30(truncated(partial_account(options.flat), int(20), int(2)))\n\
+"
+# ledger bal "/^Expenses:(?!Tax|Deductions)/" "/^Expenses:Tax:Sales/" -X $ \
+# -f ${LEDGER_FILE} --price-db ${LEDGER_PRICES} --no-total \
+# --now $NOW -b "$YEAR/01/01" -e $END --current \
+# --balance-format "\
+# %-12(abs(display_total / $MONTH))\
+# %-10((abs(display_total / $MONTH) * 12) / 365)\
+# %-12((abs(display_total / $MONTH) * 12) / 12)\
+# %-12(abs(display_total / $MONTH) * 12)\
+# %-14((abs(display_total / $MONTH) * 12) / ($withdrawal_rate / 100))\
+# %(depth_spacer)\
+# %-20(truncated(partial_account(options.flat), int(20), int(2)))\n\
+# "
+
+# YEAR=2017
+# # MONTH=07
+# MONTH=06
+
+# current_date="2017/07"
 # until_date="2017/08/01"
 # now_date="2017/07/31"
-LEDGER_FILE="/home/matt/Dropbox/journals/finances/accounting/ledger/data/general.ledger"
-LEDGER_PRICES="/home/matt/Dropbox/journals/finances/accounting/ledger/data/prices.ledger"
+# # current_date="2017/07/01"
+# # until_date="2017/08/01"
+# # now_date="2017/07/31"
+# LEDGER_FILE="/home/matt/Dropbox/journals/finances/accounting/ledger/data/general.ledger"
+# LEDGER_PRICES="/home/matt/Dropbox/journals/finances/accounting/ledger/data/prices.ledger"
 
 
-LEDGER_ACCT="Expenses"
-DATE_DISPLAY="${YEAR}/01/01"
-begin="${current_date}"
-end="${until_date}"
-now="${now_date}"
+# LEDGER_ACCT="Expenses"
+# DATE_DISPLAY="${YEAR}/01/01"
+# begin="${current_date}"
+# end="${until_date}"
+# now="${now_date}"
 
-BEGIN=$begin
-END=$end
-NOW=$now
+# BEGIN=$begin
+# END=$end
+# NOW=$now
 
 
-taxes_and_deductions=$(\
-ledger bal "/^Expenses:(Tax:(?!Sales)|Deductions)/" -X $ --invert -c \
--f ${LEDGER_FILE} --price-db ${LEDGER_PRICES} \
---now $NOW -b "$YEAR/01/01" -e $END \
---balance-format "%(display_total)\n%(display_total / $MONTH)\n" \
-| tail -n 2 \
-)
-echo "$taxes_and_deductions"
+# taxes_and_deductions=$(\
+# ledger bal "/^Expenses:(Tax:(?!Sales)|Deductions)/" -X $ --invert -c \
+# -f ${LEDGER_FILE} --price-db ${LEDGER_PRICES} \
+# --now $NOW -b "$YEAR/01/01" -e $END \
+# --balance-format "%(display_total)\n%(display_total / $MONTH)\n" \
+# | tail -n 2 \
+# )
+# echo "$taxes_and_deductions"
 # ledger bal "/^Expenses:(?!Tax|Deductions)/" "/^Expenses:Tax:Sales/" -X $ --invert -c \
 # -f $LEDGER_FILE --price-db $LEDGER_PRICES \
 # --now $NOW -b "$YEAR/01/01" -e $END \
