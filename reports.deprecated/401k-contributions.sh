08aab7b2f0aaa25c9993668fa274505b091aece4
Matt Sheehan <sheehamj@outlook.com>
2018-12-22 00:12:07 -0500

    removed old reports

--- reports.deprecated/401k-contributions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ledger bal "^Assets:Portfolio:Paychex:401k" --pivot TYPE --limit "tag(\"TYPE\")"
-
-ledger reg "^Assets:Portfolio:Paychex:401k" --limit "tag(\"TYPE\")=='TRADITIONAL'"
-
-ledger reg "^Assets:Portfolio:Paychex:401k" --limit "tag(\"TYPE\")=='MATCH'"
-
